cca5955fa9482da30b3a19cd86da32d5f298ee37
Carlos <carlos.castilla009@gmail.com>
2020-05-31 03:25:35 -0500

    Ruta de cada Heroe con su parametro 'id' - Router (http://localhost:4200/#/heroe/3)

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, IHeroe } from '../../services/heroes.services';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -12,11 +12,16 @@ export class HeroesComponent implements OnInit {
 
   heroes: IHeroe[] = [];
 
-  constructor( private _heroesService: HeroesService ) { }
+  constructor( private _heroesService: HeroesService,
+              private router: Router 
+              ) { }
 
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes);
   }
 
+  verHereo( id: number){
+    this.router.navigate( ['/heroe',id ] )
+  }
 }
